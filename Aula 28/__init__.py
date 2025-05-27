9e24e325028c4cbb1be336ad0cb94eef6a28535a
lipeeso <lipeeso021@gmail.com>
2025-05-26 21:24:16 -0300

    Expressão condicional com operador OR

---
+++ Aula 28/__init__.py
@@ -0,0 +1,25 @@
+'''Expressão condicional com operador OR
+OBS: Ele executa o primeiro valor que for True'''
+
+name = input('Digite um nome: ')
+print(name or 'Você não digitou nada')
+
+'''Modo normal'''
+# if name:
+#     print(name)
+# else:
+#     print('Você não digitou nada =(')
+
+'''Aqui ele vai pegar o primeiro valor que for True'''
+# a = 0
+# b = None
+# c = False
+# d = []
+# e = {}
+# f = 22
+# g = 'Luiz'
+
+# variavel = a or b or c or d or e or f or g
+# print(variavel)
+
+
