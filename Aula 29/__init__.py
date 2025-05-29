5aa0f17084d8babfd52b2da0af0aee5e435036d5
lipeeso <lipeeso021@gmail.com>
2025-05-28 23:49:16 -0300

    correçao

--- Aula 29/__init__.py
+++ Aula 29/__init__.py
@@ -3,7 +3,7 @@
 '''Fazer um loop que conte em ordem crescente e decrecente'''
 
 
-for numero in range(9):
-    print(numero)
+for p, numero in enumerate(range(10,1,-1)):
+    print(p,numero)
     
 
