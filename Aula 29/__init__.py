cb835297b2e3c5ff3bdc19cbca6db1d340e9b89f
lipeeso <lipeeso021@gmail.com>
2025-05-29 22:19:58 -0300

    Desafio

--- Aula 29/__init__.py
+++ Aula 29/__init__.py
@@ -1,9 +1,14 @@
 '''Exercicio de lógica'''
 
 '''Fazer um loop que conte em ordem crescente e decrecente'''
+"""
+start (opcional): O número inicial da sequência (padrão é 0).
+stop (obrigatório): O número final da sequência (a sequência irá até, mas não incluirá este número).
+step (opcional): O incremento (ou decremento) entre os números da sequência (padrão é 1).
 
+"""
 
-for p, numero in enumerate(range(10,1,-1)):
+for p, numero in enumerate(range(10,0,-1)):
     print(p,numero)
     
 
