4f0f4b7a8ea47f0090efd0020084ff91c939fb43
lipeeso <lipeeso021@gmail.com>
2025-06-09 23:14:48 -0300

    function

---
+++ Aula 32/__init__.py
@@ -0,0 +1,27 @@
+
+'''
+Funções em Python - Parte 1 
+'''
+'''Para evitar de dar erro de posicional argumento basta declarar valores padrão dentro dos paramêtros para não dar erro'''
+
+
+# def funcao(msg):
+#     print(f'Olá, {msg}')
+    
+def saudacao(msg, name):
+    print(msg, name)
+    
+
+saudacao('Olá', 'Felipe')
+    
+# def soma(a,b):
+#     print(a+b)
+    
+
+
+#funcao()
+
+
+
+#soma(10,50)
+
