e9a15cafc0c7e8cbc3e8c59baca49f95966ffd18
lipeeso <lipeeso021@gmail.com>
2025-06-12 23:07:46 -0300

    function part3

---
+++ Aula 33/__init__.py
@@ -0,0 +1,25 @@
+'''Funções def em Python - Return Part 2'''
+
+'''RASCUNHO'''
+
+# def divisao(
+#     n1,
+#     n2
+# ):
+#     if n2 == 0: 
+#         return #! Return vazio é None por padrão
+    
+#     return n1 / n2
+
+# var = divisao(10,5)
+
+# if var:   #! se dividir por zero será None por padrão
+#     print(var)
+# else:
+#     print('Nenhum valor')
+    
+
+def dump():
+    return 12
+
+print(dump(), type(dump()))
