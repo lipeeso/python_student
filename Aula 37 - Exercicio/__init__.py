991a52e32bca61471e18f210c005e1b07eec6b4e
Felipe Oliveira <lipeeso021@gmail.com>
2025-07-03 22:36:55 -0300

    func exercice

---
+++ Aula 37 - Exercicio/__init__.py
@@ -0,0 +1,8 @@
+'''
+Exercicio: 
+
+1 - Crie uma função1 que recebe a função2 como parâmetro e retorne o valor da função2 executada.
+
+
+2 - Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada. Faça a função1 executar duas funções que recebam um número diferente de argumentos.
+'''
